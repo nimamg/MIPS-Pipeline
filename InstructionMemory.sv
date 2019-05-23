c4b7b686205e794e2633a984ca4e2bc38b6da1ed
nmgtav <nima.mg@gmail.com>
2019-05-23 13:03:03 +0430

    instruction mem input name updated

--- InstructionMemory.sv
+++ InstructionMemory.sv
@@ -1,6 +1,6 @@
 `timescale 1ns/1ns
 
-module instructionMemory (input [9:0] address, output [31:0] instruction);
+module instructionMemory (input [9:0] instructionAddress, output [31:0] instruction);
     reg [31:0] insMem [0:1023];
     assign instruction = insMem[address];
 endmodule
