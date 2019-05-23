09597559b236d965669a91cfda870b04757d0237
nmgtav <nima.mg@gmail.com>
2019-05-23 21:09:52 +0430

    control inputs added, some bugs fixed, ID/EX reg, EX wires and control signals mux added

--- datapath.sv
+++ datapath.sv
@@ -1,7 +1,8 @@
 `timescale 1ns / 1ns
 
-module dataPath (input clk, rst, pcSrc, pcWrite, ifFlush, ifidWrite, stall,
-    regWrite);
+module dataPath (input clk, rst, input [1:0] pcSrc, pcWrite, ifFlush, ifidWrite, stall,
+    controlSel, regWrite, regDst, memWrite, memRead, aluSel, memToReg,
+    input [2:0] aluOp, output equal);
     // IF wires
     wire [31:0] pcOut, pcIn, IncPcOut, jmpAdr, branchAdr,instruction;
 
@@ -12,7 +13,7 @@ module dataPath (input clk, rst, pcSrc, pcWrite, ifFlush, ifidWrite, stall,
     instructionMemory instructionMem (pcOut, instruction);
     // IF stage
 
-    ifidReg (IncPcOut, instruction, ifFlush, ifidWrite, clk, rst, idPCin, idInstructionIn); // IF/ID Reg
+    ifidReg stage1Reg (IncPcOut, instruction, ifFlush, ifidWrite, clk, rst, idPCin, idInstructionIn); // IF/ID Reg
 
      // ID wires
     wire [31:0] idPCin, idInstructionIn, regWriteData, regData1, regData2;
@@ -28,7 +29,21 @@ module dataPath (input clk, rst, pcSrc, pcWrite, ifFlush, ifidWrite, stall,
     assign Rt = idInstructionIn[20:16];
     assign Rd = idInstructionIn[15:11];
     assign MemoryOffset = idInstructionIn[15:0];
-    regFile registerFile (Rs, Rt, writeAdr, regWrite, clk, rst, regWriteData, regData1, regData2);
+    regFile registerFile (Rs, Rt, writeAdr, regWriteFromWB, clk, rst, regWriteData, regData1, regData2);
+    assign {regWrite, regDst, memWrite, memRead, aluSel, memToReg, aluOp} = (controlSel == 0) ? 8'b0
+     : {regWrite, regDst, memWrite, memRead, aluSel, memToReg, aluOp}; // Control Signal mux
+     assign equal = (regData1 == regData2) ? 1'b1 : 1'b0; // branch comparator
     //ID stage
-    
+
+    idexReg stage2Reg (clk, rst, regData1, regData2, Rs, Rt, Rd, MemoryOffset,
+        regWrite, regDst, memWrite, memRead, aluSel, memToReg, aluOp, exDataIn1,
+        exDataIn2, exRsIn, exRtIn, exRdIn, exOffsetIn, exAluOpIn, exRegWriteIn, exRegDstIn,
+        exMemWriteIn, exMemReadIn, exAluSelIn, exMemToRegIn);
+
+    //EX wires
+    wire [31:0] exDataIn1, exDataIn2;
+    wire [4:0] exRsIn, exRtIn, exRdIn,;
+    wire [15:0] exOffsetIn;
+    wire [2:0] exAluOpIn;
+    wire exRegWriteIn, exRegDstIn, exMemWriteIn, exMemReadIn, exAluSelIn, exMemToRegIn;
 endmodule
