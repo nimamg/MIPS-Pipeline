029d3d1a7805a51a391a23aab6665f0617dd3a85
nmgtav <nima.mg@gmail.com>
2019-05-23 15:09:38 +0430

    The registers between the IF and ID stages added

---
+++ IFIDREG.sv
@@ -0,0 +1,21 @@
+`timescale 1ns/1ns
+
+module ifidReg (input [31:0] ifidPCin, ifidInstructionIn, input ifFlush, ifidWrite, clk, rst
+    output [31:0] ifidPCout, ifidInstructionOut);
+    always @(posedge clk, posedge rst) begin
+        if (rst) begin
+            ifidPCout = 0;
+            ifidInstructionOut = 0;
+        end
+        else if (ifFlush) begin
+            ifidInstructionOut = 0;
+        end
+        else if (ifidWrite) begin
+            ifidPCout = ifidPCin;
+            ifidInstructionOut = ifidInstructionIn;
+        end
+        else begin
+            ifidPCout = ifidPCout;
+            ifidInstructionOut = ifidInstructionOut;
+        end
+    end
