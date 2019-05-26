f47fa42f722ffe8a2056aa10b67b848587e5f97d
nmgtav <nima.mg@gmail.com>
2019-05-26 13:54:54 +0430

    last else added

--- ALU.sv
+++ ALU.sv
@@ -5,5 +5,5 @@ module ALU (input [31:0] aluInA, aluInB, input [2:0] aluOp, output [31:0] aluRes
     (aluOp == 3'b001) ? aluInA - aluInB : // sub
     (aluOp == 3'b010) ? aluInA & aluInB : // and
     (aluOp == 3'b011) ? aluInA | aluInB : // or
-    (aluOp == 3'b100) ? ((aluInA < aluInB) ? {30'b0,1'b1} : 31'b0);
-endmodule 
+    (aluOp == 3'b100) ? ((aluInA < aluInB) ? {30'b0,1'b1} : 31'b0) : aluResult;
+endmodule
