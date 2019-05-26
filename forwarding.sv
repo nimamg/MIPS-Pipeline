99c3f4821398ec42385e076b02f0af4697e32db3
nmgtav <nima.mg@gmail.com>
2019-05-26 20:16:09 +0430

    vice versa outputs

--- forwarding.sv
+++ forwarding.sv
@@ -6,15 +6,15 @@ module forwarding(input[4:0] ExMemRd, MemWbRd, Rt, Rs, input regWrite1, regWrite
         BSel = 0;
         if (regWrite1 == 1) begin
             if (Rs == ExMemRd && ExMemRd != 5'b0)
-                ASel <= 2;
+                ASel <= 1;
             if (Rt == ExMemRd && ExMemRd != 5'b0)
-                BSel <= 2;
+                BSel <= 1;
         end
         else if (regWrite1 == 1) begin
             if (Rs == MemWbRd && MemWbRd != 5'b0)
-                ASel <= 1;
+                ASel <= 2;
             if (Rt == MemWbRd && MemWbRd != 5'b0)
-                BSel <= 1;
+                BSel <= 2;
         end
     end
 endmodule
