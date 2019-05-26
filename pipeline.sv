5db70be4c1796a4c9a8f0562781dbc084b1df0ef
nmgtav <nima.mg@gmail.com>
2019-05-26 20:16:18 +0430

    some bugs fixed

--- pipeline.sv
+++ pipeline.sv
@@ -1,7 +1,7 @@
 `timescale 1ns/1ns
 
 module pipeLine(input clk, rst);
-    wire equalToControl, regDst, regWrite, memWrite, memRead, aluSel, memToReg, IFFlush, 
+    wire equalToControl, regDst, regWrite, memWrite, memRead, aluSel, memToReg, IFFlush,
     exMemReadToHazard, controlSel, ifidWrite, pcWrite, memRegWriteToFrwrd, wbRegWriteToFrwrd;
     wire[1:0] pcSrc, aSel, bSel;
     wire[2:0] aluOP;
@@ -11,16 +11,16 @@ module pipeLine(input clk, rst);
 
     dataPath dp(clk, rst, pcSrc, aSel, bSel, pcWrite, IFFlush, ifidWrite,
     controlSel, regWrite, regDst, memWrite, memRead, aluSel, memToReg,
-    aluOp, equalToControl, idRsToHazard, idRtToHazard,
+    aluOP, equalToControl, idRsToHazard, idRtToHazard,
     exRtToFrwrd_Hazard, exRsToFrwrd, memRdToFrwrd, wbRdToFrwrd, exMemReadToHazard,
     memRegWriteToFrwrd, wbRegWriteToFrwrd, instruction);
 
-    hazard hazardUnit(idRsToHazard, idRtToHazard, exRtToFrwrd_Hazard, exMemReadToHazard, 
+    hazard hazardUnit(idRsToHazard, idRtToHazard, exRtToFrwrd_Hazard, exMemReadToHazard,
     controlSel, ifidWrite, pcWrite);
 
-    forwarding forwardingUnit(memRdToFrwrd, wbRdToFrwrd, exRtToFrwrd_Hazard, exRsToFrwrd, memRegWriteToFrwrd, wbRegWriteToFrwrd, 
+    forwarding forwardingUnit(memRdToFrwrd, wbRdToFrwrd, exRtToFrwrd_Hazard, exRsToFrwrd, memRegWriteToFrwrd, wbRegWriteToFrwrd,
     aSel, bSel);
 
-    controller cntrlr(equalToControl, instruction, pcSrc, 
+    controller cntrlr(equalToControl, instruction, pcSrc,
     regDst, regWrite, memWrite, memRead, aluSel, memToReg, IFFlush, aluOP);
 endmodule
