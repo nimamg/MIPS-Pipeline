f267199af7a90a3c205fbb444706167ec75de1eb
Parsa Hoseininejad <hoseininejad1999@gmail.com>
2019-05-26 17:43:12 +0430

    r type instructions added

--- InstructionMemory.sv
+++ InstructionMemory.sv
@@ -3,10 +3,11 @@
 module instructionMemory (input [31:0] instructionAddress, output [31:0] instruction);
     reg [7:0] insMem [0:4095];
     initial begin
-        insMem[0] = 8'b10101010;
-        insMem[1] = 8'b00001111;
-        insMem[2] = 8'b01010101;
-        insMem[3] = 8'b00110011;
+        {insMem[0],insMem[1],insMem[2],insMem[3]} = 32'b10001100000000010000000000000000 ;
+        {insMem[4],insMem[5],insMem[6],insMem[7]} = 32'b10001100000000100000000000000001 ;
+        {insMem[8],insMem[9],insMem[10],insMem[11]} = 32'b00000000001000100001100000100001 ;
+        {insMem[12],insMem[13],insMem[14],insMem[15]} = 32'b00000000010000110010000000100100 ;
+        {insMem[16],insMem[17],insMem[18],insMem[19]} = 32'b00000000100000110100000000101011 ;
     end
     assign instruction = {insMem[instructionAddress],insMem[instructionAddress + 1],insMem[instructionAddress + 2],insMem[instructionAddress + 3]};
 endmodule
