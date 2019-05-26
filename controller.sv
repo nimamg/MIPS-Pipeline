d129b13e3e5e62ed578eb9786b15ce0abd9ce44f
nmgtav <nima.mg@gmail.com>
2019-05-26 15:45:09 +0430

    minor syntax bugs fixed

--- controller.sv
+++ controller.sv
@@ -1,11 +1,11 @@
 `timescale 1ns / 1ns
 
-module controller(input equal, input [31:0] instruction, output reg [1:0] pcSrc, output reg regDst, regWrite, memWrite, memRead, aluSel, memToReg, IFFlush, output reg [2:0] aluOP);
+module controller(input equal, input [31:0] instruction, output reg [1:0] pcSrc, output reg regDst, regWrite, memWrite, memRead, aluSel, memToReg, IFFlush, output reg [2:0] aluOp);
   wire[5:0] opcode, func;
   assign func = instruction[5:0];
   assign opcode = instruction[31:26];
   always @ (instruction) begin
-    {pcSrc, regDst, regWrite, memWrite, memRead, aluSel, memToReg , aluOP, IFFlush} = 12'b0;
+    {pcSrc, regDst, regWrite, memWrite, memRead, aluSel, memToReg , aluOp, IFFlush} = 12'b0;
     case (opcode)
       6'b100011: begin //lw
         memToReg = 1;
@@ -31,30 +31,30 @@ module controller(input equal, input [31:0] instruction, output reg [1:0] pcSrc,
       end
       6'b000101: begin // branchNotEq
         if (~equal) begin
-            pcsel = 1;
+            pcSrc = 1;
             IFFlush = 1;
         end
         else begin
-          pcsel = 0;
+          pcSrc = 0;
         end
       end
       6'b000000: begin //Rtype
         regDst = 1;
         regWrite = 1;
-        else if (func == 6'b100001) begin // add
-            aluop = 0;
+        if (func == 6'b100001) begin // add
+            aluOp = 0;
         end
         else if (func == 6'b100011) begin //sub
-            aluop = 1;
+            aluOp = 1;
         end
         else if (func == 6'b100100) begin //and
-            aluop = 2;
+            aluOp = 2;
         end
         else if (func == 6'b100101) begin // or
-            aluop = 3;
+            aluOp = 3;
         end
         else if (func == 6'b101011) begin // slt
-            aluop = 4;
+            aluOp = 4;
         end
         else if (func == 6'b000000) begin //nop
             regWrite = 0;
