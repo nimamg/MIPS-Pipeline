14e1b9d9bbca89857dff76c405c0b469675bb0d3
nmgtav <nima.mg@gmail.com>
2019-05-26 13:55:48 +0430

    wrong naming corrected

--- InstructionMemory.sv
+++ InstructionMemory.sv
@@ -2,5 +2,5 @@
 
 module instructionMemory (input [31:0] instructionAddress, output [31:0] instruction);
     reg [31:0] insMem [0:1023];
-    assign instruction = insMem[address];
+    assign instruction = insMem[instructionAddress];
 endmodule
