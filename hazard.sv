f6eea7fad5af8507116e0b7287980eeef0f2cdbe
nmgtav <nima.mg@gmail.com>
2019-05-31 19:24:08 +0430

    added an exception (R0 can't be changed)

--- hazard.sv
+++ hazard.sv
@@ -1,9 +1,9 @@
 `timescale 1ns/1ns
 
 module hazard(input[4:0] Rs, Rt, IdExRt, input IdExMemRead, output reg controlSel, IfIdWrite, PCWrite);
-    assign PCWrite = (IdExMemRead && (Rs == IdExRt || Rt == IdExRt)) ? 0 : 1;
-    assign IfIdWrite = (IdExMemRead && (Rs == IdExRt || Rt == IdExRt)) ? 0 : 1;
-    assign controlSel = (IdExMemRead && (Rs == IdExRt || Rt == IdExRt)) ? 0 : 1;
+    assign PCWrite = (IdExMemRead && ((Rs == IdExRt && Rs != 0) || (Rt == IdExRt && Rt != 0))) ? 0 : 1;
+    assign IfIdWrite = (IdExMemRead && ((Rs == IdExRt && Rs != 0) || (Rt == IdExRt && Rt != 0))) ? 0 : 1;
+    assign controlSel = (IdExMemRead && ((Rs == IdExRt && Rs != 0) || (Rt == IdExRt && Rt != 0))) ? 0 : 1;
     // always @* begin
     //     PCWrite = 1;
     //     IfIdWrite = 1;
